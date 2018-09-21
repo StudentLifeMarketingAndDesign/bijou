7891893ea92a74e0fc451120415c3c3c18650109
hanef200 <iluvjellybeans45@gmail.com>
2018-09-21 15:08:11 -0500

    Added an audio upload field, and displayed audio on site

---
+++ mysite/code/BlogPostExtension.php
@@ -0,0 +1,39 @@
+<?php 
+
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\File;
+
+class BlogPostExtension extends DataExtension { 
+
+	private static $has_one = array( 
+            'AudioClip' => File::class
+        );
+
+	private static $owns = array(
+		'AudioClip'
+	);
+
+        // function getCMSFields() {
+
+        //     $fields = parent::getCMSFields(); 
+
+        //     $fields->addFieldToTab(
+        //         'Root.Upload',  
+        //         $uploadField = new UploadField(
+        //             $name = 'AudioClip',
+        //             $title = 'Upload a single audio clip'
+        //         )   
+        //     );
+        //     return $fields;         
+        // }  
+
+    public function updateCMSFields(FieldList $fields) {
+       $fields->push($upload = new UploadField('AudioClip', 'Upload Podcast'));
+    } 
+   
+}
+
+
+  
