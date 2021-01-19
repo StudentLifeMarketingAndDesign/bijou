5ce6c621fa6cb002fd86599aabf0b53d97b97da2
Dustin Quam <quamsta@gmail.com>
2021-01-19 14:38:29 -0600

    added next/prev functions

--- mysite/code/ShowPage.php
+++ mysite/code/ShowPage.php
@@ -146,6 +146,21 @@ class ShowPage extends BlogPost {
 
 		return $fields;
 	}
+	public function NextPage() {
+		$page = Page::get()->filter(array(
+			'ParentID' => $this->owner->ParentID,
+			'Sort:GreaterThan' => $this->owner->Sort,
+		))->First();
+		return $page;
+	}
+
+	public function PreviousPage() {
+		$page = Page::get()->filter(array(
+			'ParentID' => $this->owner->ParentID,
+			'Sort:LessThan' => $this->owner->Sort,
+		))->Last();
+		return $page;
+	}
 
 	public function getBackgroundImageURL() {
 
